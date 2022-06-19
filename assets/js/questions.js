cb489c43d0599f483cecebd39956549a4ef40ec0
Fahrettin-Argon <103507004+Fahrettin-Argon@users.noreply.github.com>
2022-06-20 02:01:00 +0300

    Add files via upload

---
+++ assets/js/questions.js
@@ -0,0 +1,85 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [{
+        numb: 1,
+        question: "Aşağıdakilerden hangisinde temel bir website yapısınıda kullanılmaz?",
+        answer: "C",
+        options: [
+            "C",
+            "HTML",
+            "CSS",
+            "JAVASCRİPT"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Bootstrap'in en son çıkan sürüm kaçtır?",
+        answer: "Bootstrap 5",
+        options: [
+            "Bootstrap 4",
+            "Bootstrap 6",
+            "Bootstrap 5",
+            "Bootstrap 3"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Bir Websiteyi internette yayınlarken hangisine ihtiyacımız olmaz?",
+        answer: "SSL Sertifikası",
+        options: [
+            "Hosting",
+            "Domain",
+            "SSL Sertifikası",
+            "FTP Bilgileri"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Aşağıdakilerden hangisi programlama dili değildir?",
+        answer: "HTML",
+        options: [
+            "C",
+            "Python",
+            "Go",
+            "HTML"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Bir çoğumuzun bildiği minecraft oyunu hangi programlama dili ile kodlandı?",
+        answer: "Java",
+        options: [
+            "Python",
+            "Java",
+            "C",
+            "Javascript"
+        ]
+    },
+
+    {
+        numb: 6,
+        question: "Aşağıdakilerden hangisi bir veritabanı değildir?",
+        answer: "FirstSQL",
+        options: [
+            "ACCESS",
+            "MSSQL",
+            "FirstSQL",
+            "MYSQL"
+        ]
+    }
+
+
+    // you can uncomment the below codes and make duplicate as more as you want to add question
+    // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+    //   {
+    //   numb: 6,
+    //   question: "Your Question is Here",
+    //   answer: "Correct answer of the question is here",
+    //   options: [
+    //     "Option 1",
+    //     "option 2",
+    //     "option 3",
+    //     "option 4"
+    //   ]
+    // },
+];
